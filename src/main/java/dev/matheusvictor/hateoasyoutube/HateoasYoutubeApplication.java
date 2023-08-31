fca7c3d66efbb1bc4e3862462352433e1cd49633
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-31 20:00:56 -0300

    feat: inital commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/dev/matheusvictor/hateoasyoutube/HateoasYoutubeApplication.java
@@ -0,0 +1,13 @@
+package dev.matheusvictor.hateoasyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasYoutubeApplication.class, args);
+	}
+
+}
